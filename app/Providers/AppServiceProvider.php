aae384733192d125af905c053cba724503352962
camacho1980 <jorgearguello@live.com.ar>
2022-05-18 11:48:53 -0300

    correccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('REDIRECT_HTTPS')){
-            $this->app['request']->server->set('HTTPS', TRUE);
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
         }
     }
 
